6ad46d303e1e32895cbcf41fe00095fd861de1d3
Darshana Shrestha <darshanashresth0425@gmail.com>
2026-09-08 14:34:34 +1000

    added week 8

--- ICT113/Week6/Labtest.py/Q2.py
+++
@@ -1,8 +0,0 @@
-
-
-i=0
-for i in range(1,7):
-    if i==3:
-        continue
-    print(i)
-
--- ICT113/Week6/Labtest.py/Q4.py
+++
@@ -1,9 +0,0 @@
-import random
-sum = 0
-avg = 0
-for i in range(5):
-    a= random.randint(1,6)
-    sum = sum + a
-
-print("Sum:",sum)
-print("Average:",sum/5)
